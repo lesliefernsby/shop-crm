acb0de100ba3902bf3d902c8e81c0591af64792d
Leslie <leslie.fernsby@gmail.com>
2022-06-21 05:58:41 -0400

    Server Auth and Security

--- server/app.js
+++ server/app.js
@@ -1,15 +1,23 @@
+require('rootpath')();
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('helpers/error-handler');
 
 const app = express();
 const { PORT } = process.env ?? 3001;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// app.use(express.static('public'));
+// api routes
+app.use('/users', require('./controllers/users'));
 
+// global error handler
+app.use(errorHandler);
+
+// start server
 
 app.listen(process.env.PORT ?? 3001, () => { console.log(`Server started sucessfully at port ${PORT}`); }); 
---
+++ server/service/user.js
@@ -0,0 +1,52 @@
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const Role = require('../helpers/role');
+const { User } = require('../db/models');
+const bcrypt = require("bcrypt");
+const Op = require('sequelize').Op;
+const saltRounds = 10;
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById
+};
+
+async function authenticate({ username, password }) {
+    const hash = await bcrypt.hash(password, saltRounds);
+    const user = await User.findOne({ where : {
+        [Op.or]: [
+          { username: username },
+          { email: username }
+        ]
+      }, raw: true});
+
+    const pass = await bcrypt.compare(password, user.password)
+
+    console.log(user, pass);
+    if (user && pass) {
+        const token = jwt.sign({ sub: user.id, roles: user.roles }, config.secret);
+        const { password, createdAt, updatedAt, ...userWithoutPassword } = user;
+        return {
+            ...userWithoutPassword,
+            token
+        };
+    }
+}
+
+async function getAll() {
+    const users = await User.findAll({ raw: true});
+    return users.map(user => {
+        const { password, createdAt, updatedAt, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    });
+}
+
+async function getById(id) {
+    const user = await User.findOne({ where : {
+      id: id
+    }, raw: true});
+    if (!user) return;
+    const { password, createdAt, updatedAt, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
