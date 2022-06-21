72b318d67e214db0a30aa61bb9092d3f4309d3f2
Leslie <leslie.fernsby@gmail.com>
2022-06-21 08:10:40 -0400

    Front 0.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Checkout from './components/Checkout/Checkout';
+import { PrivateRoute } from './hoc/PrivateComponent';
 
 function App() {
   return (
-    <div className="main">
-      Hello World
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <PrivateRoute exact path="/checkout" element={<Checkout />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
