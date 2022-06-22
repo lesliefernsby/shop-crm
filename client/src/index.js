b34798fd9086d8dd85c4c8ee56a7ea85a421ca9c
Leslie <leslie.fernsby@gmail.com>
2022-06-22 04:50:50 -0400

    Registration

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import {  ThemeProvider } from '@mui/material/styles';
 import {theme} from './theme/theme.js'
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -16,6 +16,16 @@ export function authentication(state = initialState, action) {
         loggedIn: true,
         user: action.user
       };
+      case userConstants.REGISTER_REQUEST:
+        return {
+          loggingIn: true,
+          user: action.user
+        };
+      case userConstants.REGISTER_SUCCESS:
+        return {
+          loggedIn: true,
+          user: action.user
+        };
     case userConstants.LOGIN_FAILURE:
       return {};
     case userConstants.LOGOUT:
