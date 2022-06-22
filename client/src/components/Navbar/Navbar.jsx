fcec20ffcefd62d52a1cc528f8bd267f11577c69
OskMurphy <oskarahm@ya.ru>
2022-06-22 10:12:44 +0300

    NavBar Beta

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { AppBar, Container, Toolbar, Box, IconButton, Typography } from '@mui/material'
-import { Menu } from '@mui/icons-material'
+import { AppBar, Container, Toolbar, Box, IconButton, Typography, Button } from '@mui/material'
+import { Menu, Lock, MeetingRoom, Person } from '@mui/icons-material'
+// import useProductSearch from '../customHooks/useProductSearch'; Над переносом надо нормально подумать
 
 const Navbar = () => {
   return (
@@ -28,11 +29,46 @@ const Navbar = () => {
             variant='h6'
             component='h1'
             noWrap
-            sx={{flexGrow:1, display:{xs:'none', md:'none'}}}
+            sx={{flexGrow:1, display:{xs:'flex', md:'none'}}}
             >
               Shop_m
             </Typography>
+
+
             
+            {/* unlogged */}
+          <Button 
+          color='inherit'
+          startIcon={<Lock />}
+          href= "/login"
+          >
+          Login
+          </Button>
+
+          {/* <Button 
+          color='inherit'
+          startIcon={<ExitToApp />}
+          >
+          Sign In
+          </Button> */}
+            {/* /unlogged */}
+
+            {/* logged */}
+            <Button 
+          color='inherit'
+          startIcon={<Person />}
+          href= "/">
+          username
+          </Button>
+
+            <Button 
+          color='inherit'
+          startIcon={<MeetingRoom />}
+          href= "/checkout">
+          Logout
+          </Button>
+          {/* /logged */}
+
         </Toolbar>
       </Container>
     </AppBar>
